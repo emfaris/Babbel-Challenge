a4b975a521835098c0be67b2f57341b1b7d9f180
Muhammad Faris <faris.nazar@gmail.com>
2020-09-03 18:32:08 +0500

    First assertion passed
    After clicking the Sign In button, Login form should be displayed.
    Applied expectation that the form should be visible.

--- page_actions/homepage_action.js
+++ page_actions/homepage_action.js
@@ -1,12 +1,17 @@
-const HomePageObj = require("../page_objects/homepage_obj");
+const HomePageObj = require("../page_objects/homepage_obj")
+const LoginPageObj = require("../page_objects/loginpage_obj")
+const Help = require("../Util/helper")
 
 const HomePageActions = function(){
 
     const homePageObj = new HomePageObj();
+    const loginPageObj = new LoginPageObj();
+    const help = new Help();
     
     //Navigating to Sign In page from Homepage
     this.goToSignInPage = function(){
         homePageObj.signInBtn.click();
+        help.waitUntilReady(loginPageObj.loginForm);
 
     }
 };
--- tests/login.js
+++ tests/login.js
@@ -1,9 +1,11 @@
-const HomePageActions = require("../page_actions/homepage_action");
-const { browser } = require("protractor");
+const HomePageActions = require("../page_actions/homepage_action")
+const LoginPageObj = require("../page_objects/loginpage_obj")
+
 
 describe('Login Tests', () => {
     
     const homePageActions = new HomePageActions();
+    const loginPageObj = new LoginPageObj();
 
     beforeAll(function(){
         browser.ignoreSynchronization = true;
@@ -14,16 +16,17 @@ describe('Login Tests', () => {
 
     it("Should navigate to login page", function(){
        homePageActions.goToSignInPage();
+       expect(protractor.ExpectedConditions.visibilityOf(loginPageObj.loginForm)).toBeTruthy();
        
-    })
+    });
     
     it("Should deny login with invalid credentials", function(){
         //TO DO
-    })
+    });
 
     it("Should successfully login user with valid credentials", function(){
         //TO DP
-    })
+    });
 
 
 });
